e53664636b4781b887374081099c8f5b8312062e
Anton Jansen <antonjansen96@gmail.com>
2021-05-29 20:25:53 +0200

    add question

---
+++ CC2021/09_do_you_know_how_to_steek.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+# While everybody is monopolizing the streets, you have been busy practicing you
+# klaverjas cheating skills. You have a deck of 32 cards for a game of klaverjas
+# and managed to place the four aces on top. You've perfected the art of perfect
+# cuts and shuffles, and set out to construct a system that allows you to deal all
+# aces back to your own team. Starting and ending with a perfect cut in the middle
+# of the deck you execute 1 to 11 of the following shuffles:
+# riffle shuffle - 1 2 3 4 5 6 -> 1 4 2 5 3 6, 
+# monge shuffle - 1 2 3 4 5 6 -> 6 4 2 1 3 5, 
+# sake shuffle - 1 2 3 4 5 6 -> 3 2 1 6 5 4,
+# peel shuffle - 1 2 3 4 5 6 -> 3 4 2 5 1 6. 
+# What fraction of these shuffles will result in all aces being dealt to your
+# team? Note that the cards are dealt as 3,3,3,3,2,2,2,2,3,3,3,3, you start with
+# the player left of you, and your team mate is opposite of you.
