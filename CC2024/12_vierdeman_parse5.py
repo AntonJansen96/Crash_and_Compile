09c234868fbfe9b0e2fea8da495a66220aaf2988
Anton Jansen <antonjansen96@gmail.com>
2024-10-19 17:26:50 +0200

    solved

--- CC2024/12_vierdeman_parse5.py
+++ CC2024/12_vierdeman_parse5.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Using the extra information at your disposal, find the amount of times that a team went wet because of the fame.
-
 import json
 
 tournament = json.load(open("data/tournament_simplified.json"))
@@ -12,17 +10,16 @@ for boom in tournament:
 
     for slag in boom["rounds"]:
 
-        if not slag['wet']:
+        if not slag["wet"]:
             continue
 
         wieSpeeltId = slag["bid"]["highestBidBy"]
-        hetbod = slag["bid"]["bid"]
+        bod_origineel = slag["bid"]["bid"]
 
-        score0 = 0
-        score1 = 0
-        roem0 = 0
-        roem1 = 0
+        score0, score1 = 0, 0
+        roem0, roem1 = 0, 0
 
+        # Herbouw de score en vind de roem door door de handen heen te loopen.
         for idx in range(len(slag["playedTricks"])):
 
             hand = slag["playedTricks"][idx]
@@ -31,32 +28,21 @@ for boom in tournament:
                 roem0 += hand["honor"]
                 score0 += hand["points"]
                 if idx == 7:
-                    score0 += 10  # laatste slag is 10 punten extra
+                    score0 += 10  # Laatste slag is 10 punten extra.
             else:
                 roem1 += hand["honor"]
                 score1 += hand["points"]
                 if idx == 7:
-                    score1 += 10  # laatste slag is 10 punten extra
-
-        score0 += 0.5 * roem0  # nu hebben we de totale scores herbouwd
-        score1 += 0.5 * roem1
-
-        # Roem gaat in de .json file in twintigtallen. Het verwarrende is dat
-        # voor de score van de hele slag (voor het tournooi) roem in de volle
-        # twintigtallen wordt geteld, maar tijdens de slag zelf, als je zegmaar
-        # checkt of je nat bent, maar in tientallen. vandaar dat de daadwerkelijke
-        # score = - 0.5 * eigen roem en dat hetbod += 0.5 * vijandige roem.
+                    score1 += 10  # Laatste slag is 10 punten extra.
 
         if wieSpeeltId in [0, 2]:
-            score = score0 - 0.5 * roem0  # daadwerkelijk score
-            newbod = hetbod + 0.5 * roem1
+            score = score0
+            bod_nieuw = bod_origineel + 0.5 * roem1
         else:
-            score = score1 - 0.5 * roem1  # daadwerkelijke score
-            newbod = hetbod + 0.5 * roem0
+            score = score1
+            bod_nieuw = bod_origineel + 0.5 * roem0
 
-        
-        if (score >= hetbod) and (score < newbod):
-            print(score, roem0, roem1, hetbod, newbod)
+        if (score >= bod_origineel) and (score < bod_nieuw):
             count += 1
 
 print(count)
