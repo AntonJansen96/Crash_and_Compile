e59b3c76767b47f3c071fd7d33d65406fab7576f
Anton Jansen <antonjansen96@gmail.com>
2024-10-19 16:41:52 +0200

    solved crude

--- CC2024/11_vierdeman_parse4.py
+++ CC2024/11_vierdeman_parse4.py
@@ -10,20 +10,27 @@ for boom in tournament:
 
     for slag in boom["rounds"]:
 
-        score0 = slag["wij"]  # team 0 (players 0, 2)
-        score1 = slag["zij"]  # team 1 (players 1, 3)
-
         wieSpeeltId = slag["bid"]["highestBidBy"]
-        
-        # Roem
-
-        
         hetbod = slag["bid"]["bid"]
 
-        if wieSpeeltId in [0, 2]:  # team 0
-            score = score0
-        else:                      # team 1
-            score = score1
+        # Roem
+        roem0 = 0
+        roem1 = 0
+        for hand in slag["playedTricks"]:
+            if hand["winner"] in [0, 2]:
+                roem0 += hand["honor"]
+            else:
+                roem1 += hand["honor"]
+
+        roem0 *= 1  # 20 roem -> 20 punten voor spelende team
+        roem1 *= 1
+
+        if wieSpeeltId in [0, 2]:  # team 0 (players 0, 2)
+            score = slag["wij"] - 0.5 * roem0
+            hetbod = hetbod + 0.5 * roem1
+        else:                      # team 1 (players 1, 3)
+            score = slag["zij"] - 0.5 * roem1
+            hetbod = hetbod + 0.5 * roem0
 
         if score >= hetbod + 10:
             lafcount += 1
